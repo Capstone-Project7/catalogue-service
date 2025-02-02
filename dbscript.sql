1ba1cd94d47b2570d7d0d3ceecf8f35b5267d594
JerinJohn30 <jerinjohn3010@gmail.com>
2025-02-02 18:38:44 +0530

    Update dbscript.sql

--- dbscript.sql
+++ dbscript.sql
@@ -8,3 +8,8 @@ create table catalogue_details(
     product_image_url varchar(200),
     product_workload int);
 
+insert into catalogue_details (product_category, product_price, product_image_url, product_workload)
+    values ('Shirt', 500, 'https://images-cdn.ubuy.co.in/653d4205bf892d42fa59b8b0-men-casual-formal-shirts-slim-fit-shirt.jpg', 2),
+           ('Pants', 800, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG17fEoyTgrA7OLkaJuSIe85bC2xXMqk5d-gGebEVcBtwMM1bPopHIckjzceOumXTfIp8&usqp=CAU',2),
+           ('Formal Suit',3000,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfsjLnN36MHT-ObO6iyxfqVhFQwEVJam5nAA&s'),
+           ('Tie',250,'https://images-cdn.ubuy.co.in/64d4f3f1e9afef22ea77a467-4pcs-mens-ties-men-s-necktie-classic-tie.jpg',1);
